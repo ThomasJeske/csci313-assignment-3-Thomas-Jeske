7a7a3233e51f5b07d793b380cb233be90fc3ef4c
Thomas Jeske <user@dyn170-015.wireless-1725.ndsu.NoDak.edu>
2024-04-03 18:55:33 -0500

    Created generic list and detailed views for book and author

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -18,7 +18,7 @@ class BooksInstanceInline(admin.TabularInline):
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
     fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
-    inlines = [BooksInstanceInline]
+    
 
 # Register the admin class with the associated model
 admin.site.register(Author, AuthorAdmin)
