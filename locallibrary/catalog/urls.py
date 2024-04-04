a7d578712f3948ba1376e16ced36e956b5e56d07
Thomas Jeske <user@dyn170-015.wireless-1725.ndsu.NoDak.edu>
2024-04-03 19:41:04 -0500

    Implememtation of User authentication and permissions

--- locallibrary/catalog/urls.py
+++ locallibrary/catalog/urls.py
@@ -7,5 +7,9 @@ urlpatterns = [
     path('book/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
     path('authors/', views.AuthorListView.as_view(), name='authors'),
     path('authors/<int:pk>', views.AuthorDetailView.as_view(), name='author-detail'),
+    path('mybooks/', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
 
 ]
+# Add Django site authentication urls (for login, logout, password management)
+
+
