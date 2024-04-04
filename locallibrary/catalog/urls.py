471036462e54ac54eea7c9397491390455a18cac
Thomas Jeske <user@dyn170-015.wireless-1725.ndsu.NoDak.edu>
2024-04-03 20:05:53 -0500

    Impolemented forms and Create, Update, Delete

--- locallibrary/catalog/urls.py
+++ locallibrary/catalog/urls.py
@@ -11,5 +11,13 @@ urlpatterns = [
 
 ]
 # Add Django site authentication urls (for login, logout, password management)
+urlpatterns += [
+    path('book/<uuid:pk>/renew/', views.renew_book_librarian, name='renew-book-librarian'),
+]
+urlpatterns += [
+    path('author/create/', views.AuthorCreate.as_view(), name='author-create'),
+    path('author/<int:pk>/update/', views.AuthorUpdate.as_view(), name='author-update'),
+    path('author/<int:pk>/delete/', views.AuthorDelete.as_view(), name='author-delete'),
+]
 
 
